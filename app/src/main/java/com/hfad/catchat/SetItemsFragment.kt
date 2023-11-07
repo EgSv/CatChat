9042c20d1c7886a0183af036a2c8f8d74be612fa
EgSv <113702706+EgSv@users.noreply.github.com>
2023-11-07 11:23:19 +0300

    ссс2

---
+++ app/src/main/java/com/hfad/catchat/SetItemsFragment.kt
@@ -0,0 +1,16 @@
+package com.hfad.catchat
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class SetItemsFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_set_items, container, false)
+    }
+}
